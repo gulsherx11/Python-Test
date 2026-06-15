2e217a91a3499fc579bc03a95264d6b60ae3cc72
M. Gul Sher <sherg8662@gmail.com>
2026-06-15 10:51:15 +0500

    First

---
+++ test_calculator.py
@@ -0,0 +1,31 @@
+from calculator import sum, subtract, multiply, power, modulus, floor_divide, divide
+import pytest
+
+def test_sum():
+    assert (2+3)==5
+
+def test_subtract():
+    assert (3-5)==-2
+
+def test_multiply():
+    assert (3*5)==15
+
+def test_power():
+    assert (2**3)==8
+
+def test_modulus():
+    assert (10%3)==1
+
+def test_floor_divide():
+    assert (10//3)==3
+
+def test_divide():
+    with pytest.raises(ZeroDivisionError):
+        divide(5,0)
+        
+def test_floor_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        floor_divide(5,0)
+
+
+
